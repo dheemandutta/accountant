96bca076983151ea300be383ad6a515903cc02d0
Prasenjit Paul <prasenjitpaul100@hotmail.com>
2019-07-26 15:54:42 +0530

    AddNewCompany

---
+++ Accountant/Controllers/AddNewCompanyController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CableMan.UI.Controllers
+{
+    public class AddNewCompanyController : Controller
+    {
+        // GET: AddNewCompany
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
