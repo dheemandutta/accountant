b4a4cb19ef34df3338c47317e6790f72f25e9032
Prasenjit Paul <prasenjitpaul100@hotmail.com>
2019-08-07 19:07:32 +0530

    may-b-da-last

---
+++ Accountant.Entities/CustomerEntities.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Accountant.Entities
+{
+    public class CustomerEntities
+    {
+        public int CustomerId { get; set; }
+        public string CustomerName { get; set; }
+        public string Address { get; set; }
+        public string PanNo { get; set; }
+        public string CstNo { get; set; }
+        public string VatNo { get; set; }
+
+        public int AccountId { get; set; }
+        public int YearId { get; set; }
+        public string AccountName { get; set; }
+        public int GroupId { get; set; }
+        public string AccountCode { get; set; }
+        public Boolean isActive { get; set; }
+        public Decimal OpeningBalance { get; set; }
+        public Decimal ClosingBalance { get; set; }
+        public int CompanyId { get; set; }
+        public int ReferenceAccountId { get; set; }
+    }
+}
---
+++ Accountant.UI/Controllers/CustomerController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using Accountant.BL;
+using Accountant.Entities;
+using System.Globalization;
+using System.Web.Script.Serialization;
+
+namespace Accountant.UI.Controllers
+{
+    public class CustomerController : Controller
+    {
+        // GET: Customer
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
